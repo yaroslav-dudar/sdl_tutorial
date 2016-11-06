7fa75025df4276234b50246f019d273af33b9d40
Yaroslav <flayingfog@gmail.com>
2016-11-06 10:27:57 +0200

    tabs to spaces

--- 01_hello_SDL.cpp
+++ 01_hello_SDL.cpp
@@ -1,8 +1,6 @@
-/*This source code copyrighted by Lazy Foo' Productions (2004-2015)
-and may not be redistributed without written permission.*/
-
-//Using SDL and standard IO
+// Using SDL and standard IO
 #include <SDL2/SDL.h>
+// Using SDL2_image
 #include <SDL2/SDL_image.h>
 #include <stdio.h>
 
@@ -12,53 +10,53 @@ const int SCREEN_HEIGHT = 600;
 
 int main( int argc, char* args[] )
 {
-	//The window we'll be rendering to
-	SDL_Window* window = NULL;
-	// SDL Event handler
-	SDL_Event event;
-	//The surface contained by the window
-	SDL_Surface* image = NULL;
-	// 2D Rendering Context for Window
-	SDL_Renderer * renderer = NULL;
-	// Create Textture from existing Surface
-	SDL_Texture * texture = NULL;
+    //The window we'll be rendering to
+    SDL_Window* window = NULL;
+    // SDL Event handler
+    SDL_Event event;
+    //The surface contained by the window
+    SDL_Surface* image = NULL;
+    // 2D Rendering Context for Window
+    SDL_Renderer * renderer = NULL;
+    // Create Textture from existing Surface
+    SDL_Texture * texture = NULL;
 
-	// Application state
-	bool quit = false;
+    // Application state
+    bool quit = false;
 
-	//Initialize SDL
-	SDL_Init(SDL_INIT_VIDEO);
-	// Initialize SDL_image
-	IMG_Init(IMG_INIT_PNG);
+    //Initialize SDL
+    SDL_Init(SDL_INIT_VIDEO);
+    // Initialize SDL_image
+    IMG_Init(IMG_INIT_PNG);
 
-	//Create window
-	window = SDL_CreateWindow(
-		"SDL Tutorial",
-		SDL_WINDOWPOS_UNDEFINED,
-		SDL_WINDOWPOS_UNDEFINED,
-		SCREEN_WIDTH, SCREEN_HEIGHT,
-		SDL_WINDOW_SHOWN
-	);
+    //Create window
+    window = SDL_CreateWindow(
+        "SDL Tutorial",
+        SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
+        SCREEN_WIDTH, SCREEN_HEIGHT,
+        SDL_WINDOW_SHOWN
+    );
 
-	if( window == NULL )
-	{
-		printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
-		return 1;
-	}
+    if( window == NULL )
+    {
+        printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+        return 1;
+    }
 
-	image = IMG_Load("sprites/spritesheet.png");
-	renderer = SDL_CreateRenderer(window, -1, 0);
-	texture = SDL_CreateTextureFromSurface(renderer, image);
+    image = IMG_Load("sprites/spritesheet.png");
+    renderer = SDL_CreateRenderer(window, -1, 0);
+    texture = SDL_CreateTextureFromSurface(renderer, image);
 
-	while (!quit)
+    while (!quit)
     {
         SDL_WaitEvent(&event);
 
         switch (event.type)
         {
-	        case SDL_QUIT:
-	            quit = true;
-	            break;
+            case SDL_QUIT:
+                quit = true;
+                break;
         }
 
         // Copy a portion of the texture
@@ -67,16 +65,16 @@ int main( int argc, char* args[] )
         // Update the screen
         SDL_RenderPresent(renderer);
     }
-	
-	// Free memory
-	SDL_DestroyTexture(texture);
+    
+    // Free memory
+    SDL_DestroyTexture(texture);
     SDL_FreeSurface(image);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
 
-	//Quit SDL, IMG subsystems
-	IMG_Quit();
+    //Quit SDL, IMG subsystems
+    IMG_Quit();
     SDL_Quit();
 
-	return 0;
+    return 0;
 }
