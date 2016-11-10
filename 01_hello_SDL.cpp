55fa052634ccc4bbe3cdc6e7ad37d710d07bed22
Yaroslav <flayingfog@gmail.com>
2016-11-10 08:16:06 +0200

    add keyboard event handlers

--- 01_hello_SDL.cpp
+++ 01_hello_SDL.cpp
@@ -8,6 +8,11 @@
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
 
+struct Character {
+    int x_pos;
+    int y_pos;
+};
+
 int main( int argc, char* args[] )
 {
     //The window we'll be rendering to
@@ -38,6 +43,11 @@ int main( int argc, char* args[] )
         SDL_WINDOW_SHOWN
     );
 
+    // initialzie character
+    struct Character character;
+    character.x_pos = 10;
+    character.y_pos = 10;
+
     if( window == NULL )
     {
         printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
@@ -48,22 +58,63 @@ int main( int argc, char* args[] )
     renderer = SDL_CreateRenderer(window, -1, 0);
     texture = SDL_CreateTextureFromSurface(renderer, image);
 
+    // Set the color used for drawing operations
+    SDL_SetRenderDrawColor(renderer, 168, 230, 255, 255);
+    SDL_RenderClear(renderer);
+
     while (!quit)
     {
-        SDL_WaitEvent(&event);
+        // SDL_GetTicks() gives us the number of milliseconds
+        // that passed since the program started
+        Uint32 ticks = SDL_GetTicks();
+        Uint32 sprite = (ticks / 100) % 4;
 
-        switch (event.type)
+        SDL_Rect srcrect = { sprite * 32, 0, 32, 64 };
+        SDL_Rect dstrect = {
+            character.x_pos,
+            character.y_pos,
+            32, 64
+        };
+
+        while (SDL_PollEvent(&event))
         {
-            case SDL_QUIT:
-                quit = true;
-                break;
+            switch (event.type)
+            {
+                case SDL_QUIT:
+                    quit = true;
+                    break;
+
+                case SDL_KEYDOWN:
+                    // key press handler
+                    switch (event.key.keysym.sym)
+                    {
+                        case SDLK_LEFT:
+                            character.x_pos -= 5;
+                            break;
+                        case SDLK_RIGHT:
+                            character.x_pos += 5;
+                            break;
+                        case SDLK_UP:
+                            break;
+                        case SDLK_DOWN:
+                            break;
+                    }
+                    break;
+            }
         }
 
+        // Clearing context we drew before
+        SDL_RenderClear(renderer);
         // Copy a portion of the texture
         // to the current rendering target.
-        SDL_RenderCopy(renderer, texture, NULL, NULL);
+        SDL_RenderCopy(renderer, texture, &srcrect, &dstrect);
         // Update the screen
         SDL_RenderPresent(renderer);
+        // Sleep 20 ms
+        SDL_Delay(20);
+        //SDL_Log("SDL_Log: Hello World\n");
+        // Flush user events
+        SDL_FlushEvents(SDL_USEREVENT, SDL_LASTEVENT);
     }
     
     // Free memory
